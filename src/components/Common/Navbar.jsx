86013266d8fa7f5b09ff5bd3f5146befaad0c6b9
19yash <yashgupta19082000@gmail.com>
2023-10-10 11:48:10 +0530

    second commit updating Navbar

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -44,6 +44,7 @@ function Navbar() {
       setLoading(true)
       try {
         const res = await apiConnector("GET", categories.CATEGORIES_API)
+        if(res.data.data)
         setSubLinks(res.data.data)
       } catch (error) {
         console.log("Could not fetch Categories.", error)
